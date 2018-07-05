7d4006e1ccfa742abc19418aae6a4895d71cc52e
ajmal <ajmal@Populos-Mac-mini-3.local>
2018-07-05 18:07:47 +0530

    passing json data

--- app/src/main/java/com/example/kliq/eventattendancemobile/register/Register.java
+++ app/src/main/java/com/example/kliq/eventattendancemobile/register/Register.java
@@ -1,15 +1,73 @@
 package com.example.kliq.eventattendancemobile.register;
 
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
 import com.example.kliq.eventattendancemobile.R;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class Register extends AppCompatActivity {
+
+    EditText firstName, lastName, emailAddress, password;
+    Button registerButton;
+    RequestQueue queueVolley;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        firstName = findViewById(R.id.firstName);
+        lastName = findViewById(R.id.lastName);
+        emailAddress = findViewById(R.id.emailAddress);
+        password = findViewById(R.id.password);
+        registerButton = findViewById(R.id.registerButton);
+
+
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String fName = firstName.getText().toString();
+                final String lName = lastName.getText().toString();
+                final String email = emailAddress.getText().toString();
+                final String pass = password.getText().toString();
+
+                Response.Listener<String> responseListener = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonResponse = new JSONObject(response);
+                            boolean success = jsonResponse.getBoolean("success");
+                            if (success) {
+                                Intent intent = new Intent();
+                                Register.this.startActivity(intent);
+                            } else {
+                                AlertDialog.Builder builder = new AlertDialog.Builder(Register.this);
+                                builder.setMessage("Registration Failed")
+                                        .setNegativeButton("Retry", null)
+                                        .create()
+                                        .show();
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                };
+
+                RegisterRequest registerRequest = new RegisterRequest(fName, lName, email, pass, responseListener);
+                RequestQueue queue = Volley.newRequestQueue(Register.this);
+                queue.add(registerRequest);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/kliq/eventattendancemobile/register/RegisterRequest.java
@@ -0,0 +1,30 @@
+package com.example.kliq.eventattendancemobile.register;
+
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by ajmal on 7/5/18.
+ */
+
+public class RegisterRequest extends StringRequest{
+    private static final String REGISTER_REQUEST_URL = "http://mydomain.hostei.com/Register.php";
+    private Map<String, String> params;
+
+    public RegisterRequest(String fName, String lName, String email, String pass, Response.Listener<String> listener) {
+        super(Method.POST, REGISTER_REQUEST_URL, listener, null);
+        params = new HashMap<>();
+        params.put("fName", fName);
+        params.put("lName", lName + "");
+        params.put("email", email);
+        params.put("pass", pass);
+    }
+
+    @Override
+    public Map<String, String> getParams() {
+        return params;
+    }
+}
